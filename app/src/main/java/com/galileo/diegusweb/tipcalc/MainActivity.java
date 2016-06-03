9dae27626ecffbf1242ceefa72501798979b5ff2
Diego Rueda <diego.rueda2@gmail.com>
2016-06-02 21:00:00 -0400

    work ui

--- app/src/main/java/com/galileo/diegusweb/tipcalc/MainActivity.java
+++ app/src/main/java/com/galileo/diegusweb/tipcalc/MainActivity.java
@@ -2,6 +2,8 @@ package com.galileo.diegusweb.tipcalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_about){
+            about();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void about(){
+        //TipCalcApp
     }
 }
